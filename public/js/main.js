bef6fe722d1f4da2b85bc64a06fc3248133608e5
Jamila <jaja_mimi@hotmail.com>
2020-12-28 14:14:14 +0100

    exo2

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+// exo 1
+
+let maPhrase ="j'aime les tartines et j'en mange"
+console.log(maPhrase);
+
+let dose = "4 par jours";
+
+premierePhrase = maPhrase + " " + dose
+console.log(premierePhrase);
+
+let troisieme = 5
+let quatrieme = 35
+
+let resultat = troisieme + quatrieme
+console.log(resultat);
+
+deuxiemePhrase = maPhrase + " " +troisieme
+console.log(deuxiemePhrase);
+
+// exo 2 
+let phrase = " Salut tout le monde"
+console.log(phrase);
+
+let phrase2 = "au revoir"
+console.log(phrase2);
+
+phrase = phrase2
+
+console.log(phrase);
+
+// exo 3
+
+let number1 = 4
+let number2 = "6"
+
+resultat1 = number1 > number2
+// console.log(resultat1);
+
+resultat2 = number1 < number2  
+// console.log(resultat2);
+
+resultat3 = number1 == number2
+console.log(resultat3);
+
+resultat4 = number1 != number2
+console.log(resultat4); 
+
+resultat5 = number1 === number2
+console.log(resultat5);
+
+resultat6 = number1 !== number2
+console.log(resultat6);
+
